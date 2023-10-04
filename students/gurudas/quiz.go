559a43e786671a322ae353994fee6415caf2f727
Guru Das Srinagesh <gurooodas@gmail.com>
2023-10-03 22:21:18 -0700

    quiz: Part 1 complete, no csv package though
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- students/gurudas/quiz.go
+++ students/gurudas/quiz.go
@@ -5,9 +5,13 @@ import (
 	"bufio"
 	"os"
 	"log"
+	"strings"
+	"strconv"
 )
 
 func main() {
+	var user, ans, correct, total int;
+
 	f, err := os.Open("problems.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -17,10 +21,23 @@ func main() {
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		total++
+
+		split := strings.Split(line, ",")
+		ans, _ = strconv.Atoi(split[1])
+		fmt.Print(split[0], " = ")
+
+		fmt.Scan(&user)
+
+		if user == ans {
+			correct++;
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("You got %d out of %d questions right!\n", correct, total)
 }
